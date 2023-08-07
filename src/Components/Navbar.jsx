6c7affabd15a08415ae17408fea2d22260b4ea98
Pranav Prodduturi <Pranav@Pranavs-MacBook-Air.local>
2023-08-07 11:10:24 +0530

    commit o.1

---
+++ src/Components/Navbar.jsx
@@ -0,0 +1,33 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+   <header className="text-white bg-[#141414] body-font">
+  <div className="container mx-auto flex flex-wrap py-5 flex-col md:flex-row items-center">
+    <a className="flex title-font font-bold items-center text-white text-xl mr-2 mb-4 md:mb-0">
+      
+      Pranav Chandra
+    </a>
+    <nav className="md:mr-auto md:ml-4 md:py-1 md:pl-4 flex flex-wrap items-center text-base justify-center">
+      <a className="mr-5 hover:text-gray-900">About me</a>
+      <a className="mr-5 hover:text-gray-900">Portfolio</a>
+      <a className="mr-5 hover:text-gray-900">Skils</a>
+      <a className="mr-5 hover:text-gray-900">Milestone</a>
+      <a className="mr-5 hover:text-gray-900">Connect</a>
+    </nav>
+    <div className='flex gap-5'>
+
+    <button className="inline-flex items-center bg-gray-800 border-2 py-2 px-6 border-gray-600 focus:outline-none hover:bg-gray-200 rounded-xl text-base mt-4 md:mt-0">Resume
+     
+    </button>
+    <button className="inline-flex items-center bg-blue-700 border-2 py-2 px-6 border-gray-600 focus:outline-none hover:bg-gray-200 rounded-xl text-base mt-4 md:mt-0">Book a Call
+     
+     </button>
+    </div>
+  </div>
+</header>
+
+  )
+}
+
+export default Navbar
--- src/app/page.js
+++ src/app/page.js
@@ -1,113 +1,100 @@
-import Image from 'next/image'
+"use client"
+import Book from '@/Components/Book'
+import Footer from '@/Components/Footer'
+import Navbar from '@/Components/Navbar'
+import Portfolio from '@/Components/Portfolio'
+import Timeline from '@/Components/Timeline'
+import React from 'react'
+import Typewriter from 'typewriter-effect'
 
-export default function Home() {
+const Home = () => {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.js</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <>
+    <Navbar/>
+   <section className="bg-[#141414]">
+  <div className="container py-16 mx-auto">
+    <div className="items-center lg:flex">
+      <div className="w-full lg:w-3/5">
+        <div className="">
+        <p className="mt-3 text-gray-200 font-extrabold text-2xl">Hello</p>
+
+        {/* <h1 className="text-3xl font-semibold text-white lg:text-8xl">HELLO</h1> */}
+        <div>
+  <h1 className="text-5xl font-extrabold text-left lg:text-7xl 2xl:text-7xl">
+    <span className=" text-white ">
+      I am 
+    </span>
+  </h1>
+  <h1 className="text-5xl font-extrabold text-left lg:text-6xl 2xl:text-7xl">
+    <span className="text-transparent bg-gradient-to-tl bg-clip-text from-blue-500 to-pink-500 ">
+      Pranav Chandra
+    </span>
+  </h1>
+</div>
+<div className='text-white font-bold text-3xl mt-2'>
+
+<Typewriter
+options={{
+  strings: [' Entrepreneur','Product Designer','Growth Marketer'],
+  autoStart: true,
+  loop: true,
+}}
+/>
+</div>
+
+          <p className="mt-3 lg:pr-10 text-gray-600 dark:text-gray-400">Passionate product enthusiast with a diverse background in law and entrepreneurship. MBA candidate at Master&apos;s Union with proven success in scaling an F&B venture. Committed to crafting exceptional products and setting new standards for user satisfaction. Continuous learner and lateral leader, eager to connect with like-minded professionals</p>
+          
         </div>
       </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
+      <div className="flex items-center justify-center w-full mt-6 lg:mt-0 lg:w-2/5">
+        <img className="w-full h-full lg:max-w-lg shadow-2xl shadow-[#4b39ef] rounded-full" src="/hero.png" alt="hero.png" />
       </div>
+    </div>
+  </div>
+</section>
 
-      <div className="mb-32 grid text-center lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800 hover:dark:bg-opacity-30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore the Next.js 13 playground.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
+<Portfolio/>
+<section className="bg-[#141414]">
+  <div className="container px-6 py-16 mx-auto text-center">
+    <div className="max-w-xl mx-auto">
+      <h1 className="text-3xl font-semibold text-white lg:text-5xl">Tech Skills</h1>
+      <p className="mt-6 text-gray-500 dark:text-gray-300">
+      Always Evolving, Forever Learning
+      </p>
+      
+    </div>
+    <div className="max-w-screen-xl mx-auto mt-20 ">
+      <div className="grid grid-cols-2 gap-8 md:grid-cols-6 lg:grid-cols-7">
+        <div className="flex items-center justify-center col-span-1 md:col-span-2 lg:col-span-1">
+          <img src='/icons/1.png' className=' aspect-[1/1] w-24 ' style={{filter: 'brightness(200%) contrast(1000%) invert(100%)'}}/>
+        </div>
+        <div className="flex items-center justify-center col-span-1 md:col-span-2 lg:col-span-1">
+          <img src='/icons/2.png' className=' aspect-[1/1] w-24' style={{filter: 'brightness(200%) contrast(1000%) invert(100%)'}} />
+        </div>
+        <div className="flex items-center justify-center col-span-1 md:col-span-2 lg:col-span-1">
+          <img src='/icons/3.png' className=' aspect-[1/1] w-24' style={{filter: 'brightness(200%) contrast(1000%) invert(100%)'}} />
+        </div>
+        <div className="flex items-center justify-center col-span-1 md:col-span-2 lg:col-span-1">
+          <img src='/icons/4.png' className=' aspect-[1/1] w-24' style={{filter: 'brightness(200%) contrast(1000%) invert(100%)'}} />
+        </div>
+        <div className="flex items-center justify-center col-span-1 md:col-span-2 lg:col-span-1">
+          <img src='/icons/5.png' className=' aspect-[1/1] w-24' style={{filter: 'brightness(200%) contrast(1000%) invert(100%)'}} />
+        </div>
+        <div className="flex items-center justify-center col-span-1 md:col-span-2 lg:col-span-1">
+          <img src='/icons/6.png' className=' aspect-[1/1] w-24' style={{filter: 'brightness(200%) contrast(1000%) invert(100%)'}} />
+        </div>
+        <div className="flex items-center justify-center col-span-1 md:col-span-2 lg:col-span-1">
+          <img src='/icons/7.png' className=' aspect-[1/1] w-24' style={{filter: 'brightness(200%) contrast(1000%) invert(100%)'}} />
+        </div>
       </div>
-    </main>
+    </div>
+  </div>
+</section>
+<Timeline/>
+<Book/>
+    <Footer/>
+    </>
   )
 }
+
+export default Home
