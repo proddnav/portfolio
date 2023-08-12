5186114a829a82458256552c9c79462a2760703f
Pranav Prodduturi <Pranav@Pranavs-MacBook-Air.local>
2023-08-12 10:12:56 +0530

    update 4.0

--- src/Components/Book.jsx
+++ src/Components/Book.jsx
@@ -1,45 +1,60 @@
-import React from 'react'
+import React from 'react';
+import Slider from 'react-slick';
+import 'slick-carousel/slick/slick.css';
+import 'slick-carousel/slick/slick-theme.css';
 
 const Book = () => {
+  const settings = {
+    dots: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 1,
+        },
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+        },
+      },
+    ],
+  };
+
   return (
-    <section className="bg-[#141414]">
-  <div className="container px-6 py-10 mx-auto">
-    <h1 className="text-3xl font-semibold text-center capitalize lg:text-5xl text-white">Books That Lit My Path
-    </h1>
-    {/* <p className="mt-4 text-center text-gray-500 dark:text-gray-300">
-      Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nostrum quam voluptatibus
-    </p> */}
-    <div className="grid grid-cols-1 gap-8 mt-8 xl:mt-12 xl:gap-12 md:grid-cols-2 xl:grid-cols-3">
-      <div>
-        <img className="object-contain w-full rounded-lg" src="/books/1.jpg" alt="imges" />
-        
+    <section className="bg-[#141414] py-10">
+      <div className="container px-6 mx-auto">
+        <h1 className="text-3xl font-semibold text-center capitalize lg:text-5xl text-white mb-12">Books That Lit My Path</h1>
+        <Slider {...settings} className="space-x-8">
+          <div className="px-4">
+            <img className="object-contain w-3/4 mx-auto rounded-lg" src="/books/1.jpg" alt="imges" />
+          </div>
+          <div className="px-4">
+            <img className="object-contain w-3/4 mx-auto rounded-lg" src="/books/2.jpg" alt="imges1" />
+          </div>
+          <div className="px-4">
+            <img className="object-contain w-3/4 mx-auto rounded-lg" src="/books/3.jpg" alt="imges2" />
+          </div>
+          <div className="px-4">
+            <img className="object-contain w-3/4 mx-auto rounded-lg" src="/books/4.jpg" alt="imges3" />
+          </div>
+          <div className="px-4">
+            <img className="object-contain w-3/4 mx-auto rounded-lg" src="/books/5.jpg" alt="imges4" />
+          </div>
+          <div className="px-4">
+            <img className="object-contain w-3/4 mx-auto rounded-lg" src="/books/6.jpg" alt="imges5" />
+          </div>
+        </Slider>
       </div>
-      <div>
-        <img className="object-contain w-full rounded-lg" src="/books/2.jpg" alt="imges1" />
-        
-      </div>
-      <div>
-        <img className="object-contain w-full rounded-lg" src="/books/3.jpg" alt="imges2" />
-        
-      </div>
-      <div>
-        <img className="object-contain w-full rounded-lg" src="/books/4.jpg" alt="imges3" />
-        
-      </div>
-      <div>
-        <img className="object-contain w-full rounded-lg" src="/books/5.jpg" alt="imges4" />
-        
-      </div>
-      <div>
-        <img className="object-contain w-full rounded-lg" src="/books/6.jpg" alt="imges5" />
-        
-      </div>
-    </div>
-  </div>
-</section>
+    </section>
+  );
+};
 
-
-  )
-}
-
-export default Book
+export default Book;
--- src/app/page.js
+++ src/app/page.js
@@ -1,33 +1,56 @@
-"use client"
-import Book from '@/Components/Book'
-import Footer from '@/Components/Footer'
-import MobileNavbar from '@/Components/MobileNavbar'
-import Navbar from '@/Components/Navbar'
-import Portfolio from '@/Components/Portfolio'
-import Timeline from '@/Components/Timeline'
-import React from 'react'
-import Typewriter from 'typewriter-effect'
-import { useMediaQuery } from 'usehooks-ts'
+"use client";
+import Book from '@/Components/Book';
+import Footer from '@/Components/Footer';
+import MobileNavbar from '@/Components/MobileNavbar';
+import Navbar from '@/Components/Navbar';
+import Portfolio from '@/Components/Portfolio';
+import Timeline from '@/Components/Timeline';
+import React from 'react';
+import Typewriter from 'typewriter-effect';
+import { useMediaQuery } from 'usehooks-ts';
 
 const Home = () => {
-  const isMobile = useMediaQuery('(max-width:1024px)')
+  const isMobile = useMediaQuery('(max-width:1024px)');
   return (
     <>
-    {/* {isMobile ?(<MobileNavbar/>):(<Navbar/>)} */}
-    <MobileNavbar/>
-    <Navbar/>
-    {/* ... rest of the code ... */}
-    </section>
-    <Portfolio/>
-    <section id='skills' className="bg-[#141414]">
-    {/* ... rest of the code ... */}
-    </section>
-    <Book/>
-    <Timeline/>
-    <Book/>
-    <Footer/>
+      {/* {isMobile ?(<MobileNavbar/>):(<Navbar/>)} */}
+      <MobileNavbar />
+      <Navbar />
+      <section id='about' className="bg-[#141414]">
+        <div className="container py-16 mx-auto">
+          <div className="items-center lg:flex">
+            <div className="w-full lg:w-3/5">
+              <p className="mt-3 text-gray-200 font-extrabold text-2xl ">Hey,</p>
+              <div>
+                <h1 className="text-5xl font-extrabold text-left lg:text-7xl 2xl:text-7xl"><span className=" text-white ">I am</span></h1>
+                <h1 className="text-5xl font-extrabold text-left lg:text-6xl 2xl:text-7xl"><span className="text-transparent bg-gradient-to-tl bg-clip-text from-blue-500 to-pink-500 ">Pranav Chandra</span></h1>
+              </div>
+              <div className="text-white font-bold text-3xl mt-2">
+                <Typewriter
+                  options={{
+                    strings: ['Ex-Lawyer', 'Entrepreneur', 'Product Enthusiast'],
+                    autoStart: true,
+                    loop: true,
+                  }}
+                />
+              </div>
+              <p className="mt-3 lg:pr-10 text-gray-600 dark:text-gray-400">Passionate product enthusiast with a diverse background in law and entrepreneurship. MBA candidate at Master's Union with proven success in scaling an F&amp;B venture. Committed to crafting exceptional products and setting new standards for user satisfaction. Continuous learner and lateral leader, eager to connect with like-minded professionals</p>
+            </div>
+            <div className="flex items-center justify-center w-full mt-6 lg:mt-0 lg:w-2/5">
+              <img className="w-full h-full lg:max-w-lg shadow-2xl shadow-[#4b39ef] rounded-full" src="/hero.png" alt="hero.png" />
+            </div>
+          </div>
+        </div>
+      </section>
+      <Portfolio />
+      <section id='skills' className="bg-[#141414]">
+        {/* ... rest of the Skills content ... */}
+      </section>
+      <Book />
+      <Timeline />
+      <Footer />
     </>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
