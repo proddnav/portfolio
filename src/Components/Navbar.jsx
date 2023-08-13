1de9de3524e9f8f62e0fbc698f601aeaf12c808f
Pranav Prodduturi <Pranav@Pranavs-MacBook-Air.local>
2023-08-13 17:24:51 +0530

    update 13.0

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -11,20 +11,21 @@ const Navbar = () => {
         <nav className="md:mr-auto md:ml-4 md:py-1 md:pl-4 flex flex-wrap items-center text-base justify-center">
           <Link href='/#about' className="mr-5 hover:text-gray-300 hover:scale-110">About me</Link>
           <Link href='/#portfolio' className="mr-5 hover:text-gray-300 hover:scale-110">Portfolio</Link>
-          <Link href='/#skills' className="mr-5 hover:text-gray-300 hover:scale-110">Skills</Link>
-          <Link href='/#milestone' className="mr-5 hover:text-gray-300 hover:scale-110">Milestone</Link>
+          <Link href='/#milestone' className="mr-5 hover:text-gray-300 hover:scale-110">Milestones</Link>
           <Link href='/#connect' className="mr-5 hover:text-gray-300 hover:scale-110">Connect</Link>
           <Link href='/booknotes' className="mr-5 hover:text-gray-300 hover:scale-110">Book Notes</Link>
-          {/* Substack Link */}
           <a href="https://substack.com/@pranavchandra?utm_source=user-menu" target="_blank" rel="noopener noreferrer" className="mr-5 hover:text-gray-300 hover:scale-110">Substack</a>
         </nav>
         <div className='flex gap-5'>
-          <button className="inline-flex items-center bg-gray-800 border-2 py-2 px-6 border-gray-600 focus:outline-none hover:bg-gray-200 rounded-xl text-base mt-4 md:mt-0">Resume</button>
-          <button className="inline-flex items-center bg-blue-700 border-2 py-2 px-6 border-gray-600 focus:outline-none hover:bg-gray-200 rounded-xl text-base mt-4 md:mt-0">Book a Call</button>
+          {/* Resume Link */}
+          <a href="https://drive.google.com/file/d/17dcDBnDnvE948BU26KpXX2Ct4mOsaGiH/view?usp=sharing" target="_blank" rel="noopener noreferrer" className="inline-flex items-center bg-gray-800 border-2 py-2 px-6 border-gray-600 focus:outline-none hover:bg-gray-200 rounded-xl text-base mt-4 md:mt-0">Resume</a>
+          {/* Book a Call Link */}
+          <a href="https://calendly.com/pranav-7ge/30min" target="_blank" rel="noopener noreferrer" className="inline-flex items-center bg-blue-700 border-2 py-2 px-6 border-gray-600 focus:outline-none hover:bg-gray-200 rounded-xl text-base mt-4 md:mt-0">Book a Call</a>
         </div>
       </div>
     </header>
   )
 }
+
 
 export default Navbar
--- src/Components/Portfolio.jsx
+++ src/Components/Portfolio.jsx
@@ -1,40 +1,40 @@
 "use client"
-import React, { useState,useEffect } from 'react'
+import React, { useState, useEffect } from 'react'
 
-const projects=[
+const projects = [
   {
-    id:1,
-    title:"Monetization Features on Paytm Insider",
-    detail:" Identified new revenue opportunities by leveraging the current platform offerings. Additionally suggested high impact low effort features to achieve the desired business objective. ",
-    image:'/portfolio/paytm.png',
-    link:'https://docs.google.com/presentation/d/1TYYj6od34r6kfHFRDP9_vwkFu10oJKpiT6vGhCMGAJA/edit?usp=sharing',
-    category:"product",
-  },
-  {
-    id:2,
-    title:"App Design for Masala Tech",
-    detail:"Created a log-in page and a portfolio of brands page",
-    image:'/portfolio/figma.png',
-    link:'https://www.figma.com/proto/3MPWABksAJKpeDOS7NWU3i/Masala-Tech-Log-in?page-id=25%3A140&type=design&node-id=25-141&viewport=218%2C218%2C0.71&t=TFxErgqNFV6bg388-1&scaling=scale-down&starting-point-node-id=25%3A141&mode=design',
-    category:"wireframe",
+    id: 1,
+    title: "Monetization Features on Paytm Insider",
+    detail: "Identified new revenue opportunities by leveraging the current platform offerings. Additionally suggested high impact low effort features to achieve the desired business objective.",
+    image: '/portfolio/paytm.png',
+    link: 'https://docs.google.com/presentation/d/1TYYj6od34r6kfHFRDP9_vwkFu10oJKpiT6vGhCMGAJA/edit?usp=sharing',
+    category: "product",
   },
   {
-    id:3,
-    title:"Accelerate Your PM Journey ! Must-See PM Resources Inside",
-    detail:"A weekly newsletter covering important updates, resources necessary for aspiring product managers and product enthusiasts",
-    image:'/portfolio/news.png',
-    link:'https://docs.google.com/document/d/1sHZBGvnREWklPmULaXqKTLdE1O1f6TSJBcQK-g8FhNg/edit?usp=sharing',
-    category:"newsletter",
+    id: 2,
+    title: "App Design for Masala Tech",
+    detail: "Created a log-in page and a portfolio of brands page",
+    image: '/portfolio/figma.png',
+    link: 'https://www.figma.com/proto/3MPWABksAJKpeDOS7NWU3i/Masala-Tech-Log-in?page-id=25%3A140&type=design&node-id=25-141&viewport=218%2C218%2C0.71&t=TFxErgqNFV6bg388-1&scaling=scale-down&starting-point-node-id=25%3A141&mode=design',
+    category: "wireframe",
   },
-{
-    Id:4,
-    title:"Marco Polo Feature Introduction",
-    detail:"Keeping the business outcome in mind added new features to the existing home-page to drive user acquisition and engagement",
-    image:'/portfolio/marcopolo.png',
-    link:'https://www.figma.com/proto/ZUindRY2SNCt4tUObIwyXE/Marco_Polo_Find_A_Counsellor?page-id=0%3A1&type=design&node-id=4-75&viewport=268%2C352%2C0.58&t=9BnS1wStWIz6z0dK-1&scaling=scale-down&starting-point-node-id=1%3A2&mode=design',
-    category:"wireframe",
+  {
+    id: 3,
+    title: "Accelerate Your PM Journey! Must-See PM Resources Inside",
+    detail: "A weekly newsletter covering important updates, resources necessary for aspiring product managers and product enthusiasts",
+    image: '/portfolio/news.png',
+    link: 'https://docs.google.com/document/d/1sHZBGvnREWklPmULaXqKTLdE1O1f6TSJBcQK-g8FhNg/edit?usp=sharing',
+    category: "newsletter",
   },
-{
+  {
+    id: 4,
+    title: "Marco Polo Feature Introduction",
+    detail: "Keeping the business outcome in mind added new features to the existing home-page to drive user acquisition and engagement",
+    image: '/portfolio/marcopolo.png',
+    link: 'https://www.figma.com/proto/ZUindRY2SNCt4tUObIwyXE/Marco_Polo_Find_A_Counsellor?page-id=0%3A1&type=design&node-id=4-75&viewport=268%2C352%2C0.58&t=9BnS1wStWIz6z0dK-1&scaling=scale-down&starting-point-node-id=1%3A2&mode=design',
+    category: "wireframe",
+  },
+  {
     id: 5,
     title: "Marco Polo",
     detail: "Strategically enhanced Marco Polo's platform, integrating mental health features to boost user acquisition and engagement.",
@@ -43,63 +43,56 @@ const projects=[
     category: "product",
   }
 ]
- 
+
 const Portfolio = () => {
-  const [data,setData] = useState(projects)
-  const [button,setButton] = useState({
-    wireframe:false,
-    product:true,
-    newsletter:false
+  const [data, setData] = useState(projects)
+  const [button, setButton] = useState({
+    wireframe: false,
+    product: true,
+    newsletter: false
   })
-  const FilterData = (category)=>{
-    const filter = projects.filter((item)=>item.category==category)
-    console.log(category,filter)
+  const FilterData = (category) => {
+    const filter = projects.filter((item) => item.category == category)
     setData(filter)
-    setButton({wireframe:false,
-      product:false,
-      newsletter:false,[category]:true})
+    setButton({ wireframe: false, product: false, newsletter: false, [category]: true })
   }
   useEffect(() => {
-    const filter = projects.filter((item)=>item.category=="product")
+    const filter = projects.filter((item) => item.category == "product")
     setData(filter)
   }, []);
   return (
- <section id='portfolio' className="bg-[#141414]">
-  <div className="container px-6 py-10 mx-auto">
-    <h1 className="text-2xl font-semibold text-center capitalize lg:text-5xl text-white">My Portfolio</h1>
-    <p className="max-w-2xl mx-auto my-6 text-center text-gray-300">
-    I believe in getting my hands dirty to build out of the ordinary.
-    </p>
-    <div className="flex items-center justify-center">
-      <div className="flex items-center p-1 md:gap-5 gap-2 rounded-xl">
-        <button onClick={()=>{FilterData('wireframe')}} className={`px-4 py-2 text-sm border border-blue-400 font-medium text-white capitalize hover:bg-blue-600 ${button.wireframe && 'bg-blue-600'} md:py-2 rounded-md md:px-6`}>Wireframe</button>
-        <button onClick={()=>{FilterData('product')}} className={`px-4 md:py-2 text-sm border border-blue-400 font-medium text-white capitalize hover:bg-blue-600 ${button.product && 'bg-blue-600'} md:py-2 rounded-md md:px-6`}>Product Portfolio</button>
-        <button onClick={()=>{FilterData('newsletter')}} className={`px-4 py-2 text-sm border border-blue-400 font-medium text-white capitalize hover:bg-blue-600 ${button.newsletter && 'bg-blue-600'} md:py-2 rounded-md md:px-6`}>Newsletters</button>
-      </div>
-    </div>
-    <div class="grid grid-cols-1 gap-8 md:grid-cols-2 py-10 xl:grid-cols-3">
-           {data.map((item)=>(
+    <section id='portfolio' className="bg-[#141414]">
+      <div className="container px-6 py-10 mx-auto">
+        <h1 className="text-2xl font-semibold text-center capitalize lg:text-5xl text-white">My Portfolio</h1>
+        <p className="max-w-2xl mx-auto my-6 text-center text-gray-300">
+          Crafting user-first innovative solutions with a blend of creativity and strategy.
+        </p>
+        <div className="flex items-center justify-center">
+          <div className="flex items-center p-1 md:gap-5 gap-2 rounded-xl">
+            <button onClick={() => { FilterData('wireframe') }} className={`px-4 py-2 text-sm border border-blue-400 font-medium text-white capitalize hover:bg-blue-600 ${button.wireframe && 'bg-blue-600'} md:py-2 rounded-md md:px-6`}>Wireframe</button>
+            <button onClick={() => { FilterData('product') }} className={`px-4 md:py-2 text-sm border border-blue-400 font-medium text-white capitalize hover:bg-blue-600 ${button.product && 'bg-blue-600'} md:py-2 rounded-md md:px-6`}>Product Portfolio</button>
+            <button onClick={() => { FilterData('newsletter') }} className={`px-4 py-2 text-sm border border-blue-400 font-medium text-white capitalize hover:bg-blue-600 ${button.newsletter && 'bg-blue-600'} md:py-2 rounded-md md:px-6`}>Newsletters</button>
+          </div>
+        </div>
+        <div className="grid grid-cols-1 gap-8 md:grid-cols-2 py-10 xl:grid-cols-3">
+          {data.map((item) => (
             <div className=' ' key={item.id}>
-            <a href={item.link} target='_blank'>
-  <img className="object-fit object-center w-full h-64 lg:h-80 aspect-video" src={item.image} alt />
-  </a>
-  <div className="p-2 rounded-b-md bg-gray-600">
-    <h1 className=" text-xl font-semibold text-white">
-      {item.title}
-    </h1>
-    <p className="mt-2 text-gray-400">
-      {item.detail}
-    </p>
-    
-  </div>
-</div>
-           ))}
-
+              <a href={item.link} target='_blank'>
+                <img className="object-fit object-center w-full h-64 lg:h-80 aspect-video" src={item.image} alt />
+              </a>
+              <div className="p-2 rounded-b-md bg-gray-600">
+                <h1 className=" text-xl font-semibold text-white">
+                  {item.title}
+                </h1>
+                <p className="mt-2 text-gray-400">
+                  {item.detail}
+                </p>
+              </div>
             </div>
-            
-  </div>
-</section>
-
+          ))}
+        </div>
+      </div>
+    </section>
   )
 }
 
